921a3c331fb1e3b960e1ee40ce56a10ea3693c05
yashverma <yash.cverma@gmail.com>
2021-09-29 02:20:30 -0700

    added . and % functionality

--- src/components/App.js
+++ src/components/App.js
@@ -85,6 +85,28 @@ const App = () => {
             setOperation('div');
             return;
         }
+
+        if(content === '.') {
+            if(displayValue.includes('.'))
+                return;
+            
+            setDisplayValue(displayValue + '.');
+            return;
+        }
+
+        if(content === '±') {
+            if(displayValue !== '0'){
+                setDisplayValue(displayValue * (-1));
+            }
+            return;
+        }
+
+        if(content === '%') {
+            if(displayValue !== '0'){
+                setDisplayValue(displayValue / 100);
+            }
+            return;
+        }
         
 
         if(content === "=") {
@@ -94,28 +116,34 @@ const App = () => {
             switch (operation) {
                 case 'add': setDisplayValue((buffer + num).toString());
                             setOperation('');
+                            setBuffer(null);
                             return;
             
                 case 'sub': setDisplayValue((buffer - num).toString());
                             setOperation('');
+                            setBuffer(null);
                             return;
 
                 case 'mul': setDisplayValue((buffer * num).toString());
                             setOperation('');
+                            setBuffer(null);
                             return;
 
                 case 'div': setDisplayValue((buffer / num).toString());
                             setOperation('');
+                            setBuffer(null);
                             return;
 
                 default: return;
             }
         }
 
-        setDisplayValue(parseFloat(num + content).toString());
+        if(displayValue[displayValue.length - 1] === '.'){
+            setDisplayValue(displayValue + content);
+        } else {
+            setDisplayValue(parseFloat(num + content).toString());
+        }
     };
-
-    
 
     return (
         <div className="app">
